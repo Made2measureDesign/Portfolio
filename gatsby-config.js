1d20d61259879084eba0aef0225e277a573f9f4b
Russo Sanakoev <madetomeasurelondon@gmail.com>
2020-08-26 15:58:46 +0100

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Sanakoev Portfolio`,
   },
   plugins: [
     {
